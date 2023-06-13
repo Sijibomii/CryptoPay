b1621821a66cb9a1c4fdcf03cad538d24677a43e
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-06-13 13:49:49 +0100

    users refactor

--- server/core/db/users.go
+++ server/core/db/users.go
@@ -39,30 +39,30 @@ func findUserByEmail(conn *gorm.DB, email string) (models.User, error) {
 	return users[0], nil
 }
 
-func findUserById(conn *gorm.DB, id uuid.UUID) (models.User, error) {
+func findUserById(conn *gorm.DB, id uuid.UUID) models.User {
 	user := models.User{}
 	if err := conn.Where("id = ?", id).First(&user).Error; err != nil {
-		return user, err
+		panic(err)
 	}
 
-	return user, nil
+	return user
 }
 
-func findUserByResetToken(conn *gorm.DB, token uuid.UUID) (models.User, error) {
+func findUserByResetToken(conn *gorm.DB, token uuid.UUID) models.User {
 	var users []models.User
 	if err := conn.Where("reset_token = ?", token).Find(&users).Error; err != nil {
-		return users[0], err
+		panic(err)
 	}
 
-	return users[0], nil
+	return users[0]
 }
 
-func activateUser(conn *gorm.DB, token uuid.UUID) (models.User, error) {
+func activateUser(conn *gorm.DB, token uuid.UUID) models.User {
 	var user models.User
 	now := time.Now()
 
 	if err := conn.Where("verification_token = ? AND is_activated = ? AND verification_token_expires_at > ?", token, false, now).First(&user).Error; err != nil {
-		return user, err
+		panic(err)
 	}
 
 	user.Is_verified = true
@@ -70,39 +70,39 @@ func activateUser(conn *gorm.DB, token uuid.UUID) (models.User, error) {
 	result := conn.Update(user)
 
 	if result.Error != nil {
-		return models.User{}, result.Error
+		panic(result.Error)
 	}
 	if result.RowsAffected == 0 {
-		return models.User{}, gorm.ErrRecordNotFound
+		panic(gorm.ErrRecordNotFound)
 	}
-	return user, nil
+	return user
 }
 
-func deleteUser(conn *gorm.DB, id uuid.UUID) (models.User, error) {
+func deleteUser(conn *gorm.DB, id uuid.UUID) models.User {
 	result := conn.Delete(&models.User{}, id)
 	if result.Error != nil {
-		return models.User{}, result.Error
+		panic(result.Error)
 	}
 	if result.RowsAffected == 0 {
-		return models.User{}, fmt.Errorf("no record found with ID %d", id)
+		panic(fmt.Errorf("no record found with ID %d", id))
 	}
-	return models.User{}, nil
+	return models.User{}
 }
 
-func deleteExpiredUser(conn *gorm.DB, email string) (models.User, error) {
+func deleteExpiredUser(conn *gorm.DB, email string) models.User {
 	var user models.User
 	now := time.Now()
 
 	if err := conn.Where("email = ? AND is_activated = ? AND verification_token_expires_at < ?", email, false, now).First(&user).Error; err != nil {
-		return user, err
+		panic(err)
 	}
 
 	result := conn.Delete(user)
 	if result.Error != nil {
-		return models.User{}, result.Error
+		panic(result.Error)
 	}
 	if result.RowsAffected == 0 {
-		return models.User{}, gorm.ErrRecordNotFound
+		panic(gorm.ErrRecordNotFound)
 	}
-	return user, nil
+	return user
 }
