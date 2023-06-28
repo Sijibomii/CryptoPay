3b48ab0e83b39386088cf58b26555b5f4bf68c00
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-06-28 09:04:28 +0100

    more api client functions

--- server/blockchain_client/bitcoin/api_client.go
+++ server/blockchain_client/bitcoin/api_client.go
@@ -1,33 +1,36 @@
 package bitcoin
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strconv"
 )
 
 type BlockchainClient struct {
-	Url string
+	BCUrl string
+	BSUrl string
 }
 
 type BlockCountStatsResponse struct {
-	Data struct {
-		Blocks int `json:"blocks"`
-	} `json:"data"`
+	Blocks int `json:"height"`
 }
 
-func New(url string) *BlockchainClient {
+func New(bc_url, bs_url string) *BlockchainClient {
 	return &BlockchainClient{
-		Url: url,
+		BCUrl: bc_url,
+		BSUrl: bs_url,
 	}
 }
 
+// https://api-r.bitcoinchain.com/v1/status
 func (client *BlockchainClient) Block_count_endpoint() string {
-	baseURL, _ := url.Parse(client.Url)
-	u := baseURL.ResolveReference(&url.URL{Path: fmt.Sprintf("/bitcoin/stats")})
+	baseURL, _ := url.Parse(client.BCUrl)
+	u := baseURL.ResolveReference(&url.URL{Path: fmt.Sprintf("/status")})
 	return u.String()
 }
 
@@ -64,7 +67,214 @@ func (client *BlockchainClient) get_block_count() (int, error) {
 	}
 
 	// Extract the number of blocks
-	numBlocks := stats.Data.Blocks
+	numBlocks := stats.Blocks
 
 	return numBlocks, nil
+}
+
+type Block struct {
+	Hash       string   `json:"hash"`
+	Height     int      `json:"height"`
+	PrevBlock  string   `json:"prev_block"`
+	NextBlock  string   `json:"next_block"`
+	MerkleRoot string   `json:"mrkl_root"`
+	Tx         []string `json:"tx"`
+	TxCount    int      `json:"tx_count"`
+	Reward     string   `json:"reward"`
+	Fee        string   `json:"fee"`
+	VoutSum    string   `json:"vout_sum"`
+	IsMain     bool     `json:"is_main"`
+	Version    string   `json:"version"`
+	Difficulty string   `json:"difficulty"`
+	Size       int      `json:"size"`
+	Bits       string   `json:"bits"`
+	Nonce      string   `json:"nonce"`
+	Time       int      `json:"time"`
+}
+
+// https://api-r.bitcoinchain.com/v1/block/0000000000000000000fa2283ae08d7e4b4da949c19a40b68c4d0e1267647250/withTx
+func (client *BlockchainClient) Get_Block_endpoint(block_hash string) string {
+	baseURL, _ := url.Parse(client.BCUrl)
+	u := baseURL.ResolveReference(&url.URL{Path: fmt.Sprintf("/blocks/%s/withTx", block_hash)})
+	return u.String()
+}
+
+func (client *BlockchainClient) Get_Block(block_hash string) (*Block, error) {
+
+	resp, err := http.Get(client.Get_Block_endpoint(block_hash))
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var data []Block
+
+	err = json.Unmarshal(body, &data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(data) > 0 {
+		return &data[0], nil
+	}
+
+	return nil, errors.New("empty response")
+}
+
+// https://api-r.bitcoinchain.com/v1/block/{blockHeight}/withTx
+func (client *BlockchainClient) Get_Block_with_height_endpoint(block_height int) string {
+	baseURL, _ := url.Parse(client.BCUrl)
+	u := baseURL.ResolveReference(&url.URL{Path: fmt.Sprintf("/blocks/%s/withTx", strconv.Itoa(block_height))})
+	return u.String()
+}
+
+func (client *BlockchainClient) Get_Block_with_height(block_height int) (*Block, error) {
+	resp, err := http.Get(client.Get_Block_with_height_endpoint(block_height))
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var data []Block
+
+	err = json.Unmarshal(body, &data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(data) > 0 {
+		return &data[0], nil
+	}
+
+	return nil, errors.New("empty response")
+}
+
+func (client *BlockchainClient) Get_Transaction_by_hash_endpoint(tx_hash string) string {
+	baseURL, _ := url.Parse(client.BSUrl)
+	u := baseURL.ResolveReference(&url.URL{Path: fmt.Sprintf("/tx/%s/", tx_hash)})
+	return u.String()
+}
+
+type Transaction struct {
+	TxID     string  `json:"txid"`
+	Version  int     `json:"version"`
+	LockTime int     `json:"locktime"`
+	Vin      []Vin   `json:"vin"`
+	Vout     []Vout  `json:"vout"`
+	Size     int     `json:"size"`
+	Weight   int     `json:"weight"`
+	Fee      float64 `json:"fee"`
+	Status   Status  `json:"status"`
+}
+
+type Vin struct {
+	TxID       string  `json:"txid"`
+	Vout       int     `json:"vout"`
+	Prevout    Prevout `json:"prevout"`
+	ScriptSig  string  `json:"scriptsig"`
+	IsCoinbase bool    `json:"is_coinbase"`
+	Sequence   uint32  `json:"sequence"`
+}
+
+type Prevout struct {
+	ScriptPubKey        string  `json:"scriptpubkey"`
+	ScriptPubKeyAsm     string  `json:"scriptpubkey_asm"`
+	ScriptPubKeyType    string  `json:"scriptpubkey_type"`
+	ScriptPubKeyAddress string  `json:"scriptpubkey_address"`
+	Value               float64 `json:"value"`
+}
+
+type Vout struct {
+	ScriptPubKey        string  `json:"scriptpubkey"`
+	ScriptPubKeyAsm     string  `json:"scriptpubkey_asm"`
+	ScriptPubKeyType    string  `json:"scriptpubkey_type"`
+	ScriptPubKeyAddress string  `json:"scriptpubkey_address"`
+	Value               float64 `json:"value"`
+}
+
+type Status struct {
+	Confirmed   bool   `json:"confirmed"`
+	BlockHeight int    `json:"block_height"`
+	BlockHash   string `json:"block_hash"`
+	BlockTime   int    `json:"block_time"`
+}
+
+func (client *BlockchainClient) Get_Transaction_By_Hash_height(tx_hash string) (*Transaction, error) {
+
+	resp, err := http.Get(client.Get_Transaction_by_hash_endpoint(tx_hash))
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var data Transaction
+
+	err = json.Unmarshal(body, &data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &data, nil
+}
+
+func (client *BlockchainClient) BroadcastTransaction_endpoint() string {
+	baseURL, _ := url.Parse(client.BSUrl)
+	u := baseURL.ResolveReference(&url.URL{Path: fmt.Sprintf("/tx/")})
+	return u.String()
+}
+
+func (client *BlockchainClient) BroadcastTransaction(rawTx string) error {
+	url := client.BroadcastTransaction_endpoint()
+	// Create a JSON payload containing the raw transaction
+	payload := []byte(fmt.Sprintf(`{"tx": "%s"}`, rawTx))
+
+	// Send a POST request to the Esplora API
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(payload))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	// Check the response status code
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to broadcast transaction: %s", resp.Status)
+	}
+
+	// Print the response body
+	fmt.Println(string(body))
+
+	return nil
 }
