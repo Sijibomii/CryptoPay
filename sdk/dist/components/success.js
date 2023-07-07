18f3aac033b2e020698c67ceb96884fa4ee21917
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-07-07 03:54:59 +0100

    published successfully

---
+++ sdk/dist/components/success.js
@@ -0,0 +1,42 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Success = void 0;
+var components_1 = require("./components");
+var Success = /** @class */ (function (_super) {
+    __extends(Success, _super);
+    function Success() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Success.prototype.onMounted = function () {
+        var _this = this;
+        this.modal.element.querySelector(".btn-close").classList.remove("disabled");
+        var dataset = this.dataset;
+        this.apiClient
+            .createVoucher(dataset.payment.id, dataset.sessionToken)
+            .then(function (data) {
+            _this.onSuccess(data.voucher);
+        });
+    };
+    Success.prototype.render = function () {
+        var currency = this.dataset.currency;
+        var template = "\n                <div class=\"modal-content simple ".concat(currency, "\">\n                    <div class=\"icon-check\"></div>\n                    <div class=\"title\">Sucessfully Completed</div>\n                    <p class=\"message\">\n                        We have confirmed your payment\n                    </p>\n                </div>\n            ");
+        this.element.innerHTML = template.trim();
+    };
+    return Success;
+}(components_1.Component));
+exports.Success = Success;
