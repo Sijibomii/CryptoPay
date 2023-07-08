268ae3f5222e580887a8c380987dd8c4521d6c2f
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-07-08 04:00:55 +0100

    tailwind init

--- examples/ecom-app/src/App.tsx
+++ examples/ecom-app/src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home';
 
+import './App.css'
 function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
+        <Routes>
           <Route>
 
           </Route>
-          <Route  path='/' component={Home} exact/>
-        </Switch>
+          <Route  path='/' Component={Home} />
+        </Routes>
       </Router>
     </div>
   );
--- examples/ecom-app/src/pages/Home.tsx
+++ examples/ecom-app/src/pages/Home.tsx
@@ -1,10 +1,111 @@
 
-
-
 const Home = () => {
 
     return (
-        <></>
+        <div className="max-w-6xl mx-auto bg-black">
+            <div className="grid grid-cols-1 md:grid-cols-2 py-4">
+                {/* priducts  */}
+                <div className="grid grid-cols-1 md:grid-cols-3 px-2">
+
+                    <div className="flex flex-col">
+                        <div className="img">
+                            <figure>
+                                <img className="" src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3000&q=80" alt="shirt-1"/>
+                            </figure>
+                        </div>
+                        <div className="py-2 flex flex-col">
+                            <h3>White Shirt</h3>
+                            <h4>$ 10.00</h4>
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart-fill" viewBox="0 0 16 16"> <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/> </svg>
+                            <p>Add to cart</p>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col">
+                        <div className="img">
+                            <figure>
+                                <img className="" src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3000&q=80" alt="shirt-1"/>
+                            </figure>
+                        </div>
+                        <div className="py-2 flex flex-col">
+                            <h3>White Shirt</h3>
+                            <h4>$ 10.00</h4>
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart-fill" viewBox="0 0 16 16"> <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/> </svg>
+                            <p>Add to cart</p>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col">
+                        <div className="img">
+                            <figure>
+                                <img className="" src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3000&q=80" alt="shirt-1"/>
+                            </figure>
+                        </div>
+                        <div className="py-2 flex flex-col">
+                            <h3>White Shirt</h3>
+                            <h4>$ 10.00</h4>
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart-fill" viewBox="0 0 16 16"> <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/> </svg>
+                            <p>Add to cart</p>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col">
+                        <div className="img">
+                            <figure>
+                                <img className="" src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3000&q=80" alt="shirt-1"/>
+                            </figure>
+                        </div>
+                        <div className="py-2 flex flex-col">
+                            <h3>White Shirt</h3>
+                            <h4>$ 10.00</h4>
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart-fill" viewBox="0 0 16 16"> <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/> </svg>
+                            <p>Add to cart</p>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col">
+                        <div className="img">
+                            <figure>
+                                <img className="" src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3000&q=80" alt="shirt-1"/>
+                            </figure>
+                        </div>
+                        <div className="py-2 flex flex-col">
+                            <h3>White Shirt</h3>
+                            <h4>$ 10.00</h4>
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart-fill" viewBox="0 0 16 16"> <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/> </svg>
+                            <p>Add to cart</p>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col">
+                        <div className="img">
+                            <figure>
+                                <img className="" src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3000&q=80" alt="shirt-1"/>
+                            </figure>
+                        </div>
+                        <div className="py-2 flex flex-col">
+                            <h3>White Shirt</h3>
+                            <h4>$ 10.00</h4>
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart-fill" viewBox="0 0 16 16"> <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/> </svg>
+                            <p>Add to cart</p>
+                        </div>
+                    </div>
+                </div>
+                {/* cart */}
+            </div>
+        </div>
     )
 }
 
