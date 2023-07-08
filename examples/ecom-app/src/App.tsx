685820c80109012b1b67d1cdadd6a468d5489754
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-07-08 03:08:36 +0100

    home page init

--- examples/ecom-app/src/App.tsx
+++ examples/ecom-app/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route>
+
+          </Route>
+          <Route  path='/' component={Home} exact/>
+        </Switch>
+      </Router>
     </div>
   );
 }
